e9d5de3141760b27a44c41d0bc2cbb2e60b6a7bb
Dmitry K <ex0hunt@yandex.ru>
2020-01-22 20:23:15 +0300

    NOISSUE: add tags model

--- api/models.py
+++ api/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 
+class Tags(models.Model):
+    name = models.CharField(max_length=250)
+
+
 class Entity(models.Model):
     slug = models.SlugField(unique=True)
     title = models.CharField(max_length=250)
     text = models.TextField()
     creation_date = models.DateTimeField(auto_now_add=True)
     edit_date = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tags)
