d8b33f53115586be93a49169ae834e358610fdd8
Dmitry Katargin <d.katargin@corp.mail.ru>
2020-11-20 19:59:09 +0300

    add tags to post list

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,20 @@
 from rest_framework import serializers
-from .models import Entity
+from .models import Entity, Tag
 
 preview_length = 500
+
+
+class EntityTagsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('name',)
 
 
 class EntitySerializer(serializers.ModelSerializer):
     preview = serializers.SerializerMethodField()
 
-    def get_preview(self, obj):
+    @staticmethod
+    def get_preview(obj):
         result = obj.text[:preview_length]
         if len(obj.text) > preview_length:
             result += '...'
@@ -15,7 +22,7 @@ class EntitySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Entity
-        fields = ('slug', 'title', 'preview', 'creation_date', 'edit_date')
+        fields = ('slug', 'title', 'preview', 'creation_date', 'edit_date', 'tags')
 
 
 class EntityDetailSerializer(serializers.ModelSerializer):
